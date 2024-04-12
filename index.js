1e5496b4fba062ebde0b09d7eb39cfa897566386
e21687592 <smill22813475@gmail.com>
2024-04-12 10:22:26 +0800

    first

---
+++ index.js
@@ -0,0 +1,68 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth-routes");
+const profileRoutes = require("./routes/profile-routes");
+require("./config/passport");
+const session = require("express-session");
+const passport = require("passport");
+const flash = require("connect-flash");
+const path = require("path");
+const port = process.env.PORT || 8080;
+
+// 连接MongoDB
+mongoose
+  .connect(process.env.MONGODB_CONNECTION)
+  .then(() => {
+    console.log("连接到 MongoDB...");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+// 设置Middlewares以及模板引擎
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+// 设置routes
+app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
+
+app.get("/", (req, res) => {
+  return res.render("index", { user: req.user });
+});
+
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
